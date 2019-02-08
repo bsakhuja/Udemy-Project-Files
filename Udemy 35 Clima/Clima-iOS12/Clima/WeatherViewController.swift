2ade2856a0194877eb8b9688009511504e651279
Brian Sakhuja <briansakhuja@gmail.com>
2019-02-08 15:39:54 -0800

    Clima progress

--- Udemy 35 Clima/Clima-iOS12/Clima/WeatherViewController.swift
+++ Udemy 35 Clima/Clima-iOS12/Clima/WeatherViewController.swift
@@ -38,6 +38,8 @@ class WeatherViewController: UIViewController, CLLocationManagerDelegate {
         locationManager.delegate = self
         locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
         locationManager.requestWhenInUseAuthorization()
+        
+        locationManager.startUpdatingLocation()
     
         
         
@@ -82,11 +84,27 @@ class WeatherViewController: UIViewController, CLLocationManagerDelegate {
     
     
     //Write the didUpdateLocations method here:
-    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations[locations.count - 1]
+        
+        if location.horizontalAccuracy > 0 {
+            locationManager.stopUpdatingLocation()
+            
+//            print("Latitude: \(location.coordinate.latitude), longitude: \(location.coordinate.longitude)")
+
+            let latitude = String(location.coordinate.latitude)
+            let longitude = String(location.coordinate.longitude)
+            
+            let params : [String : String] = ["lat" : latitude, "lon" : longitude, "appid" : APP_ID]
+        }
+    }
     
     
     //Write the didFailWithError method here:
-    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error)
+        cityLabel.text = "Location Unavailable"
+    }
     
     
 
