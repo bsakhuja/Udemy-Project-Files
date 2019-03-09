5f04954c5688c117506a8fec073fcd710ae490d4
Brian Sakhuja <briansakhuja@gmail.com>
2019-03-09 13:50:27 -0800

    Finished Udemy 49

--- Udemy 49 Twitter Sentiment/Twittermenti-iOS12/Twittermenti/ViewController.swift
+++ Udemy 49 Twitter Sentiment/Twittermenti-iOS12/Twittermenti/ViewController.swift
@@ -11,7 +11,7 @@ import SwifteriOS
 import CoreML
 import SwiftyJSON
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
     
     @IBOutlet weak var backgroundView: UIView!
     @IBOutlet weak var textField: UITextField!
@@ -21,25 +21,82 @@ class ViewController: UIViewController {
     
     let swifter = Swifter(consumerKey: TWITTER_CONSUMER_KEY, consumerSecret: TWITTER_CONSUMER_SECRET)
 
+    let tweetCount = 100
+    let language = "en"
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        let prediction = try! sentimentClassifier.prediction(text: "sentiment")
-        
+        self.textField.delegate = self
     }
 
     @IBAction func predictPressed(_ sender: Any) {
-        
-        let searchText = textField.text ?? "twitter"
-        
-        swifter.searchTweet(using: searchText, lang: "en", count: 100, tweetMode: .extended, success: { (results, metadata) in
-//            let json =  JSO
-        }) { (error) in
-            print("there was an error searching tweets")
-        }
-    
-    
+        fetchTweets()
     }
     
+    func fetchTweets() {
+        if let searchText = textField.text {
+            swifter.searchTweet(using: searchText, lang: language, count: tweetCount, tweetMode: .extended, success: { (results, metadata) in
+                
+                var tweets = [TweetSentimentClassifierInput]()
+                
+                for i in 0..<self.tweetCount {
+                    if let tweet = results[i]["full_text"].string {
+                        let tweetSentimentClassifierInput = TweetSentimentClassifierInput(text: tweet)
+                        tweets.append(tweetSentimentClassifierInput)
+                    }
+                }
+                
+                self.makePrediction(with: tweets)
+            
+            }) { (error) in
+                print("there was an error searching tweets")
+            }
+        }
+        
+    }
+    
+    func makePrediction(with tweets: [TweetSentimentClassifierInput]) {
+        do {
+            let predictions = try self.sentimentClassifier.predictions(inputs: tweets)
+            var sentimentScore = 0
+            for prediction in predictions {
+                let sentiment = prediction.label
+                
+                if sentiment == "Pos" {
+                    sentimentScore += 1
+                } else if sentiment == "Neg" {
+                    sentimentScore -= 1
+                }
+            }
+            
+            updateUI(with: sentimentScore)
+        } catch {
+            fatalError("There was an error making a batch prediction: \(error)")
+        }
+    }
+    
+    func updateUI(with sentimentScore: Int) {
+        if sentimentScore > 20 {
+            self.sentimentLabel.text = "😍"
+        } else if sentimentScore > 10 {
+            self.sentimentLabel.text = "😀"
+        } else if sentimentScore > 0 {
+            self.sentimentLabel.text = "🙂"
+        } else if sentimentScore > -10 {
+            self.sentimentLabel.text = "😐"
+        } else if sentimentScore > -20 {
+            self.sentimentLabel.text = "😟"
+        } else {
+            self.sentimentLabel.text = "😖"
+        }
+    }
+    
+    // MARK: - TextFieldDelegate
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        self.view.endEditing(true)
+        return false
+    }
 }
 
